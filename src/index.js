85ba040d01b3a471dc738e6978472a8363659735
Apple <apple@Apples-MacBook-Pro.local>
2022-12-08 16:49:25 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express =  require('express');
+const app =  express();
+const port =  process.env.PORT  ||  8080
+// Route to be tested
+app.get('/', (req, res) => {
+    return res.status(200).json({ nome:  'Mohsin Khan' });
+});
+
+// Application running on the door
+let server = app.listen(port, () => {
+    console.log(`Application running on ${port}`);
+});
+module.exports  = server;
